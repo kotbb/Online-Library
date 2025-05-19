92de253e209287812c24e50202fc04976d5c5479
Mohamed Nabil Mohamed Abusarea <mnbil0680@gmail.com>
2025-05-19 16:15:18 +0300

    zero for unavilable books

--- OnlineLibrary/project/library_app/models.py
+++ OnlineLibrary/project/library_app/models.py
@@ -36,7 +36,7 @@ class Book(models.Model):
     category = models.CharField(max_length=50, choices=CATEGORY_CHOICES)
     description = models.TextField(blank=True, null=True)    
     status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='available')
-    count = models.PositiveIntegerField(default=1, validators=[MinValueValidator(1), MaxValueValidator(100)])
+    count = models.PositiveIntegerField(default=1, validators=[MinValueValidator(0), MaxValueValidator(100)])
     cover_image = models.FileField(upload_to='book_covers/', null=True, blank=True)
     #added_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     added_date = models.DateTimeField(auto_now_add=True)
